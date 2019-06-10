1868c62919712726beeae3df3548fe8fde314e80
Jeffrey Shek <jeffshek@gmail.com>
2019-06-10 11:44:40 -0400

    add cloudflare api data fetch

--- open/core/utilities/cloudflare.py
+++ open/core/utilities/cloudflare.py
@@ -1,21 +1,42 @@
-from django.conf import settings
+import pandas as pd
 import requests
+from django.conf import settings
 
-# import pandas as pd
+start_date_default = "2019-05-04T00:00:00Z"
+end_date_default = "2019-05-11T00:00:00Z"
+
+CLOUDFLARE_HEADERS = {
+    "X-Auth-Email": settings.CLOUDFLARE_EMAIL,
+    "X-Auth-Key": settings.CLOUDFLARE_API_KEY,
+}
+
+CLOUDFLARE_PREFIX = "https://api.cloudflare.com/client/v4/zones"
 
 
-def get_dashboard_history():
-    cloudflare_headers = {
-        "X-Auth-Email": settings.CLOUDFLARE_EMAIL,
-        "X-Auth-Key": settings.CLOUDFLARE_API_KEY,
-    }
-
-    cloudflare_prefix = "https://api.cloudflare.com/client/v4/zones"
+def get_dashboard_history(start_date=start_date_default, end_date=end_date_default):
     cf_url = (
-        f"{cloudflare_prefix}/{settings.CLOUDFLARE_SENRIGAN_ZONE_ID}/analytics/dashboard?since=2019-05-01T12:23:00Z&until=2019-05-05T12:23:00Z&continuous=true"  # noqa: E501
+        f"{CLOUDFLARE_PREFIX}/{settings.CLOUDFLARE_SENRIGAN_ZONE_ID}/analytics/dashboard?since={start_date}&until={end_date}&"  # noqa: E501
     )
-
-    response = requests.get(cf_url, headers=cloudflare_headers)
+    response = requests.get(cf_url, headers=CLOUDFLARE_HEADERS)
 
     data = response.json()
-    return data
+
+    unique_values = []
+    unique_dates = []
+
+    timeseries = data["result"]["timeseries"]
+    for daily_snapshot in timeseries:
+
+        date_start = daily_snapshot["since"]
+        uniques = daily_snapshot["uniques"]
+
+        uniques_all = uniques["all"]
+
+        # create two lists holding the dates and values to load into a pd.Series
+        unique_dates.append(date_start)
+        unique_values.append(uniques_all)
+
+    series = pd.Series(data=unique_values, index=unique_dates)
+    series.to_excel("exports/cloudflare_export_may.xlsx")
+
+    return series
