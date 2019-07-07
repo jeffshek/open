b389cf9a99854309f5646a3e6f38a20e738a8ad2
Jeffrey Shek <jeffshek@gmail.com>
2019-07-07 00:12:39 -0400

    remove manifest issues

--- config/settings/production.py
+++ config/settings/production.py
@@ -73,7 +73,7 @@ DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
 GS_BUCKET_NAME = env("DJANGO_GCP_STORAGE_BUCKET_NAME")
 GS_DEFAULT_ACL = "publicRead"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATIC
 # ------------------------
