dd1e7a505eb790711fd04104eced3a5d8b338664
Jeffrey Shek <jeffshek@gmail.com>
2019-07-13 15:46:35 -0500

    switch from gunicorn to daphene

--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -5,5 +5,8 @@ set -o nounset
 
 
 python /app/manage.py collectstatic --noinput
+
 # default to 4 workers
-/usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:5000 --chdir=/app --workers=4
+#/usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:5000 --chdir=/app --workers=4
+
+/usr/local/bin/daphne config.asgi:application -b 0.0.0.0:5000
