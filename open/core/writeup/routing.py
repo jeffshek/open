8e228177b4abb0335de48699056d7b6e8fa65abf
Jeffrey Shek <jeffshek@gmail.com>
2019-07-13 16:34:44 -0500

    sort of progress

--- open/core/writeup/routing.py
+++ open/core/writeup/routing.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r"^ws/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer)
+    url(r"^ws/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
+    url(r"^wss/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
 ]
