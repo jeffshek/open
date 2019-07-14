88c152e99d0f200c9fc85049c92369bc6ff7f56c
Jeffrey Shek <jeffshek@gmail.com>
2019-07-13 19:44:18 -0500

    fix confusing websocket urls

--- open/core/writeup/routing.py
+++ open/core/writeup/routing.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r"^ws/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
-    url(r"^wss/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
+    url(r"^websockets/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer)
 ]
