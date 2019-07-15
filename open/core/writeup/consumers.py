9df48cf8cae311d74dc5cce04160ab185f637619
Jeffrey Shek <jeffshek@gmail.com>
2019-07-14 22:04:03 -0500

    make changes to test load balancer

--- open/core/writeup/consumers.py
+++ open/core/writeup/consumers.py
@@ -1,5 +1,6 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer, JsonWebsocketConsumer
 import json
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -31,3 +32,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
+
+
+class WriteUpSessionConsumer(JsonWebsocketConsumer):
+    def connect(self):
+        group_name = self.scope["url_route"]["kwargs"]["session_uuid"]
+        self.group_name_uuid = "session_%s" % group_name
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name_uuid, self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name_uuid, self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name_uuid, {"type": "chat_message", "message": message}
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
+
+        self.send(text_data=json.dumps({"message": message}))
--- open/core/writeup/routing.py
+++ open/core/writeup/routing.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 
 from . import consumers
 
+# this is prefixed with a /ws/ for load balancers to redirect traffic
 websocket_urlpatterns = [
-    url(r"^websockets/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer)
+    url(r"^ws/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
+    url(
+        r"^ws/writeup/session/(?P<session_uuid>[^/]+)/$",
+        consumers.WriteUpSessionConsumer,
+    ),
 ]
