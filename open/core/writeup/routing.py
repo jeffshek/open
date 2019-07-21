43d455aa8908a44db8b94d159e4603f6f8a37bbb
Jeffrey Shek <jeffshek@gmail.com>
2019-07-20 20:14:39 -0500

    additions of consumer, refactor and setup proper routes

--- open/core/writeup/routing.py
+++ open/core/writeup/routing.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-
-from . import consumers
+from open.core.writeup import consumers
 
 # this is prefixed with a /ws/ for load balancers to redirect traffic
 websocket_urlpatterns = [
-    url(r"^ws/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
+    # url(r"^ws/chat/(?P<room_name>[^/]+)/$", consumers.ChatConsumer),
     url(
         r"^ws/writeup/session/(?P<session_uuid>[^/]+)/$",
         consumers.WriteUpSessionConsumer,
-    ),
+    )
 ]
