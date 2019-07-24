26dd999443069a5ec14ef88a1e97dae9683cb68a
Jeffrey Shek <jeffshek@gmail.com>
2019-07-23 23:26:01 -0500

    think through newlines

--- open/core/writeup/caches.py
+++ open/core/writeup/caches.py
@@ -7,7 +7,7 @@ def get_cache_key_for_gpt2_parameter(prompt, batch_size, length, temperature, to
     # have to encode into bytes first
     # just do this for redis's memory not to take up too much space
     # there's no speed difference in using shorter keys though, redis is too good
-    prompt_encoded = prompt.encode("utf-8")
+    prompt_encoded = prompt.strip().encode("utf-8")
     prompt_hash = hashlib.md5(prompt_encoded).hexdigest()
 
     cache_key = f"{app}_{prompt_hash}_{batch_size}_{length}_{temperature}_{top_k}"
--- open/core/writeup/tests/test_caches.py
+++ open/core/writeup/tests/test_caches.py
@@ -14,6 +14,34 @@ class TestWriteUpCaches(TestCase):
         cache_key = get_cache_key_for_gpt2_parameter(
             prompt, batch_size, length, temperature, top_k
         )
-        expected_result = "writeup_f400147c906266814c3028ebcccdad32_5_10_0.7_40"
+        expected_result = "writeup_6a8d5bebad2295c438c3c26c28813f80_5_10_0.7_40"
+
+        self.assertEqual(cache_key, expected_result)
+
+    def test_cache_key_with_new_line(self):
+        prompt = "Testing A Cache Key\n"
+        batch_size = 5
+        length = 10
+        temperature = 0.7
+        top_k = 40
+
+        cache_key = get_cache_key_for_gpt2_parameter(
+            prompt, batch_size, length, temperature, top_k
+        )
+        expected_result = "writeup_6a8d5bebad2295c438c3c26c28813f80_5_10_0.7_40"
+
+        self.assertEqual(cache_key, expected_result)
+
+    def test_cache_key_with_space(self):
+        prompt = "Testing A Cache Key\n   "
+        batch_size = 5
+        length = 10
+        temperature = 0.7
+        top_k = 40
+
+        cache_key = get_cache_key_for_gpt2_parameter(
+            prompt, batch_size, length, temperature, top_k
+        )
+        expected_result = "writeup_6a8d5bebad2295c438c3c26c28813f80_5_10_0.7_40"
 
         self.assertEqual(cache_key, expected_result)
