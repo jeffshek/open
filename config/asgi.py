c85d42d47befb51b2af064dd602b4e745e25ac6c
Jeffrey Shek <jeffshek@gmail.com>
2019-07-26 19:44:44 -0500

    Add Sentry on ASGI

--- config/asgi.py
+++ config/asgi.py
@@ -2,15 +2,20 @@
 ASGI entrypoint. Configures Django and then runs the application
 defined in the ASGI_APPLICATION setting.
 """
+import os
+import django
+import environ
 
 import sentry_sdk
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
-
-import os
-import django
 from channels.routing import get_default_application
 
+env = environ.Env()
+SENTRY_DSN = env("SENTRY_DSN")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 django.setup()
+sentry_sdk.init(dsn=SENTRY_DSN)
+
 application = get_default_application()
+application = SentryAsgiMiddleware(application)
