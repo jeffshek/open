38fefbbd7ca8674846900385755afcc7d8fa39b8
Jeffrey Shek <jeffshek@gmail.com>
2019-08-05 12:04:46 -0400

    Major Pass At Making Sharing and Saving Options Viable

--- open/core/writeup/constants.py
+++ open/core/writeup/constants.py
@@ -4,3 +4,42 @@ class WriteUpResourceEndpoints:
 
 
 GPT2_END_TEXT_STRING = "<|endoftext|>"
+
+
+class StaffVerifiedShareStates:
+    # admin would review any unverified issues and if they're bad
+    # put them as fail and just not show them
+    VERIFIED_FAIL = "verified_fail"
+    VERIFIED_PASS = "verified_pass"
+    # when someone marks something as bad, put this into this state
+    # if something has been reported multiple times (more than 3), change
+    # the state to more likely bad
+    UNVERIFIED_ISSUE_MULTIPLE = "unverified_issue_multiple"
+    UNVERIFIED_ISSUE = "unverified_issue"
+    UNVERIFIED = "unverified"
+
+
+STAFF_VERIFIED_SHARE_STATE_CHOICES = [
+    (StaffVerifiedShareStates.VERIFIED_FAIL, "Verified Fail"),
+    (StaffVerifiedShareStates.VERIFIED_PASS, "Verified Pass"),
+    # the default state is unverified, shouldn't have to review most things
+    # but when something is reported as an issue, put it in here
+    (StaffVerifiedShareStates.UNVERIFIED_ISSUE_MULTIPLE, "Unverified Issue Multiple"),
+    (StaffVerifiedShareStates.UNVERIFIED_ISSUE, "Unverified Issue"),
+    (StaffVerifiedShareStates.UNVERIFIED, "Unverified"),
+]
+
+
+class PromptShareStates:
+    UNSHARED = "unshared"
+    # access only if you have special link
+    PUBLISHED_LINK_ACCESS_ONLY = "published_link_access_only"
+    # everyone has access
+    PUBLISHED = "published"
+
+
+PROMPT_SHARE_STATES_CHOICES = [
+    (PromptShareStates.UNSHARED, "UNSHARED"),
+    (PromptShareStates.PUBLISHED_LINK_ACCESS_ONLY, "PUBLISHED_LINK_ACCESS_ONLY"),
+    (PromptShareStates.PUBLISHED, "PUBLISHED"),
+]
