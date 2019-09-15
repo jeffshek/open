dc90fefe287b253464eec1988b26757fa6c7fcbe
Jeffrey Shek <jeffshek@gmail.com>
2019-09-15 04:20:03 -0400

    Test Celery and Sentry Exceptions Route Correctly

--- open/core/tasks.py
+++ open/core/tasks.py
@@ -1,6 +1,12 @@
 from config.celery_app import app
 
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 @app.task(serializer="json")
 def check_services_running():
-    print("working!")
+    logger.exception("Check Celery Will Route to Sentry")
+    raise ValueError("Check Raised Exceptions Route to Sentry")
