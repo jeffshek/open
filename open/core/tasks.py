5067a9146a64874dee6520fbe62b2f4cffa03d2b
Jeffrey Shek <jeffshek@gmail.com>
2020-06-14 19:00:10 -0500

    Remove Removed Endpoint since GPT2 Large is slow, and Legal is Not Useful

--- open/core/tasks.py
+++ open/core/tasks.py
@@ -22,10 +22,10 @@ def check_services_running():
     }
 
     endpoints = [
-        # settings.GPT2_MEDIUM_API_ENDPOINT,
-        settings.GPT2_LARGE_API_ENDPOINT,
+        settings.GPT2_MEDIUM_API_ENDPOINT,
+        # settings.GPT2_LARGE_API_ENDPOINT,
         settings.GPT2_MEDIUM_HP_API_ENDPOINT,
-        settings.GPT2_MEDIUM_LEGAL_API_ENDPOINT,
+        # settings.GPT2_MEDIUM_LEGAL_API_ENDPOINT,
         settings.GPT2_MEDIUM_RESEARCH_API_ENDPOINT,
         settings.GPT2_MEDIUM_COMPANIES_API_ENDPOINT,
     ]
