0e083fda290c6067e2ad29788c5d26aac1532e83
Jeffrey Shek <jeffshek@gmail.com>
2020-07-04 21:08:49 -0500

    Add Signup History to UserModels

--- open/users/models.py
+++ open/users/models.py
@@ -1,11 +1,22 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, UUIDField
+from django.db.models import CharField, UUIDField, BooleanField
 from django.utils.translation import ugettext_lazy as _
+
+from open.constants import SIGNED_UP_FROM_DETAILS_CHOICE
 
 
 class User(AbstractUser):
     # First Name and Last Name do not cover name patterns around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
     uuid = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, unique=True)
+
+    # used to determine where a user signed up from
+    signed_up_from = CharField(
+        choices=SIGNED_UP_FROM_DETAILS_CHOICE, default="", blank=True, max_length=255
+    )
+
+    # for specific features that are specific to an app
+    is_writeup_user = BooleanField(default=False)
+    is_betterself_user = BooleanField(default=False)
