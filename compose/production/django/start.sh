c795e7e3e98df58c46178ad7eb740d7982418708
Jeffrey Shek <jeffshek@gmail.com>
2020-07-05 00:15:43 -0500

    Add No Input to Migrations

--- compose/production/django/start.sh
+++ compose/production/django/start.sh
@@ -5,6 +5,7 @@ set -o nounset
 
 
 python /app/manage.py collectstatic --noinput
+python manage.py migrate --no-input
 
 # default to 4 workers
 #/usr/local/bin/gunicorn config.wsgi --bind 0.0.0.0:5000 --chdir=/app --workers=4
