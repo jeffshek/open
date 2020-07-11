25ed6a7c6c55c1fa1818b97dcd3734f387b04f0f
Jeffrey Shek <jeffshek@gmail.com>
2020-07-11 14:38:58 -0500

    Fix After DataMigration

--- open/users/tests/test_tasks.py
+++ open/users/tests/test_tasks.py
@@ -2,12 +2,14 @@ from celery.result import EagerResult
 from django.test import TestCase
 
 from open.users.factories import UserFactory
+from open.users.models import User
 
 from open.users.tasks import get_users_count
 
 
 class TestUserCeleryTasks(TestCase):
     def test_user_count(self):
+        User.objects.all().delete()
         """A basic test to execute the get_users_count Celery task."""
         UserFactory.create_batch(3)
 
