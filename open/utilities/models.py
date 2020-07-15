1543042dfbbee32cf6528906081dee5ce6b2e0fa
Jeffrey Shek <jeffshek@gmail.com>
2020-07-14 22:43:50 -0500

    More Shuffling

--- open/utilities/models.py
+++ open/utilities/models.py
@@ -11,12 +11,23 @@ from open.users.models import User
 class BaseModel(TimeStampedModel):
     # doing this lets you insert records and modify the timestamps of created
     created = DateTimeField(default=timezone.now, editable=False, blank=True)
-    uuid = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, unique=True)
-
     # modified is inherited from TimeStampedModel
+    uuid = UUIDField(primary_key=False, default=uuid.uuid4, editable=False, unique=True)
 
     class Meta:
         abstract = True
+
+    def __repr__(self):
+        has_name_attr = getattr(self, "name", "")
+        if has_name_attr:
+            label = f"ID {self.id} | {self._meta.verbose_name.title()} - {self.name}"
+        else:
+            label = f"ID {self.id} | {self._meta.verbose_name.title()}"
+
+        return label
+
+    def __str__(self):
+        return self.__repr__()
 
     @property
     def uuid_str(self):
@@ -36,15 +47,3 @@ class BaseModelWithUserGeneratedContent(BaseModel):
         kwargs = {"uuid": instance_uuid}
         update_url = reverse(self.RESOURCE_NAME, kwargs=kwargs)
         return update_url
-
-    def __repr__(self):
-        has_name_attr = getattr(self, "name", "")
-        if has_name_attr:
-            label = f"ID {self.id} | {self._meta.verbose_name.title()} - {self.name}"
-        else:
-            label = f"ID {self.id} | {self._meta.verbose_name.title()}"
-
-        return label
-
-    def __str__(self):
-        return self.__repr__()
