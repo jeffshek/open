ea6e3c32997cfa1e75d722e64bcb64a723dc2435
Jeffrey Shek <jeffshek@gmail.com>
2020-07-16 13:39:57 -0500

    Minor Debug Helper

--- open/users/admin.py
+++ open/users/admin.py
@@ -31,7 +31,7 @@ class UserAdmin(auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "email", "is_superuser"]
+    list_display = ["username", "email", "is_superuser", "created", "modified"]
     search_fields = ["name", "email"]
     inlines = [TokenInline]
     actions = [create_demo_fixtures]
