b51aa4ae7409508cb89a3fca256505ca5f675f17
Jeffrey Shek <jeffshek@gmail.com>
2020-09-05 22:04:49 -0500

    Add Supplement Stack Functionality to Log Views

--- open/core/betterself/tests/views/test_supplement_log_views.py
+++ open/core/betterself/tests/views/test_supplement_log_views.py
@@ -9,6 +9,8 @@ from open.core.betterself.constants import (
 from open.core.betterself.factories import (
     SupplementLogFactory,
     SupplementFactory,
+    SupplementStackFactory,
+    SupplementStackCompositionFactory,
 )
 from open.core.betterself.models.supplement_log import SupplementLog
 from open.core.betterself.tests.mixins.resource_mixin import (
@@ -147,6 +149,42 @@ class TestSupplementLogViews(BetterSelfResourceViewTestCaseMixin, TestCase):
 
         display_name = response.data["display_name"]
         self.assertTrue("8.5 days ago" in display_name, display_name)
+
+    def test_create_supplement_log_with_supplement_stack(self):
+        """
+        dpy test open.core.betterself.tests.views.test_supplement_log_views.TestSupplementLogViews.test_create_supplement_log_with_supplement_stack --keepdb
+        """
+        # a hidden feature, not really restful, but allow a user to send a supplement_stack_uuid
+        # to create a set of supplements taken at the same time
+
+        supplements = SupplementFactory.create_batch(3, user=self.user_1)
+
+        stack = SupplementStackFactory(user=self.user_1)
+
+        compositions = []
+        for supplement in supplements:
+            composition = SupplementStackCompositionFactory(
+                user=self.user_1, supplement=supplement, stack=stack, quantity=2
+            )
+            compositions.append(composition)
+
+        stack_uuid = stack.uuid
+
+        utc_now = get_utc_now()
+        post_data = {
+            "supplement_uuid": str(stack_uuid),
+            "time": utc_now.isoformat(),
+            "quantity": 5,
+        }
+
+        response = self.client_1.post(self.url, data=post_data)
+        self.assertEqual(response.status_code, 200, response.data)
+
+        for supplement in supplements:
+            matching_log = SupplementLog.objects.get(
+                supplement=supplement, quantity=10, user=self.user_1, time=utc_now
+            )
+            self.assertIsNotNone(matching_log)
 
 
 class TestSupplementLogGetUpdateDelete(
