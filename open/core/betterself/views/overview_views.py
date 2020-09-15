5833cf6574f9bb7d5530bb06384cd3a8597c3526
Jeffrey Shek <jeffshek@gmail.com>
2020-09-14 22:48:47 -0500

    Self PR Comments

--- open/core/betterself/views/overview_views.py
+++ open/core/betterself/views/overview_views.py
@@ -18,28 +18,6 @@ class OverviewView(APIView):
         start_period = serialize_date_to_user_localized_datetime(start_date, user)
         end_period = serialize_end_date_to_user_localized_datetime(end_date, user)
 
-        # if period == "daily":
-        #     end_period = start_period
-        # elif period == "weekly":
-        #     end_period = get_time_relative_units_forward(start_period, weeks=1)
-        # elif period == "monthly":
-        #     end_period = get_time_relative_units_forward(start_period, months=1)
-        # elif period == "yearly":
-        #     end_period = get_time_relative_units_forward(start_period, years=1)
-        # else:
-        #     raise ValueError(f"Invalid Period {period}")
-
-        # set the end to a datetime at 11:59 PM
-        # end_period = datetime(
-        #     year=end_period.year,
-        #     month=end_period.month,
-        #     day=end_period.day,
-        #     hour=23,
-        #     minute=59,
-        #     second=59,
-        #     tzinfo=user.timezone,
-        # )
-
         sleep_data = get_overview_sleep_data(
             user, start_period=start_period, end_period=end_period
         )
@@ -52,7 +30,6 @@ class OverviewView(APIView):
         )
 
         response = {
-            # "period": period,
             # change it back to date, so it doesn't look super confusing on api response ...
             "start_period": start_period.date().isoformat(),
             "end_period": end_period.date().isoformat(),
